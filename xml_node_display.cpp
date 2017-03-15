f4eac0bda4cb1fe5eff71335525ba26275aed28a
Jerome Dockes <jerome@dockes.org>
2017-03-15 09:44:31 +0100

    fix all text turning into links after one link clicked in node display

--- xml_node_display.cpp
+++ xml_node_display.cpp
@@ -16,12 +16,12 @@ namespace xml_viewer
 
     void XML_node_display::display_node_text(const QTreeWidgetItem* new_node)
     {
-        setText(highlighted_text(new_node));
+        setHtml(highlighted_text(new_node));
     }
 
     QString highlighted_text(const QTreeWidgetItem* item)
     {
-        auto text = QString{item->text(0)};
+        QString text{item->text(0)};
 
         if(item->data(0, User_roles::is_element).toBool()){
             text.replace(QRegularExpression("(\\S*)=(\\S*)"),
@@ -31,9 +31,11 @@ namespace xml_viewer
                     QString("<b><font color=blue>\\1</font></b>"));
         }
 
-        return text.replace(
+        text.replace(
                 QRegularExpression("(https?://\\S+)"),
                 QString("<a href='\\1'>\\1</a>"));
+
+        return QString("<html>%1</html>").arg(text);
     }
 
 
